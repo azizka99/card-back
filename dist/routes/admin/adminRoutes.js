25a72613e919d930ab2830ba34e38162adf9b92a
azizka.ibragimov <azizka.ibragimov@gmail.com>
2026-01-02 10:43:48 +0100

    live feed added for testing

--- dist/routes/admin/adminRoutes.js
+++ dist/routes/admin/adminRoutes.js
@@ -5,8 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const dbConnection_1 = __importDefault(require("../../constants/dbConnection"));
 const adminRoutes = express_1.default.Router();
 adminRoutes.get("/dashboard", (0, express_async_handler_1.default)(async (req, res) => {
     res.render("adminDashboard.ejs");
+}));
+adminRoutes.get("/live-feed", (0, express_async_handler_1.default)(async (req, res) => {
+    const steam_cards = await dbConnection_1.default.steam_card.findMany({
+        include: {
+            app_user: true,
+            tag: true
+        },
+        take: 100,
+        orderBy: { created_at: "desc" }
+    });
+    res.render("live-feed", {
+        steam_cards,
+        latestTs: steam_cards[0]?.created_at?.toISOString?.() || null,
+    });
 }));
 exports.default = adminRoutes;
--- src/routes/admin/adminRoutes.ts
+++ src/routes/admin/adminRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
+import prisma from "../../constants/dbConnection";
 
 
 const adminRoutes = express.Router();
@@ -10,6 +11,21 @@ adminRoutes.get("/dashboard", expressAsyncHandler(async (req, res) => {
 }));
 
 
+adminRoutes.get("/live-feed", expressAsyncHandler(async (req, res) => {
+    const steam_cards = await prisma.steam_card.findMany({
+        include: {
+            app_user: true,
+            tag: true
+        },
+        take: 100,
+        orderBy: { created_at: "desc" }
+    });
+
+    res.render("live-feed", {
+        steam_cards,
+        latestTs: steam_cards[0]?.created_at?.toISOString?.() || null,
+    });
+}));
 
 
 
