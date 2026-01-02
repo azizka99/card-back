3c7572a2c447c6e88ff879975c62f7db000db9a3
azizka.ibragimov <azizka.ibragimov@gmail.com>
2026-01-02 13:09:03 +0100

    testtt

--- src/routes/admin/adminRoutes.ts
+++ src/routes/admin/adminRoutes.ts
@@ -65,22 +65,22 @@ adminRoutes.get("/live-feed", expressAsyncHandler(async (req, res) => {
     });
 }));
 
-adminRoutes.get("/download:tag_id", expressAsyncHandler(async (req, res) => {
-    const items = await prisma.steam_card.findMany({
-        where: {
-            tag_id: req.params.tag_id
-        },
-        select: {
-            barcode: true,
-            activation_code: true
-        }
-    });
+// adminRoutes.get("/download:tag_id", expressAsyncHandler(async (req, res) => {
+//     const items = await prisma.steam_card.findMany({
+//         where: {
+//             tag_id: req.params.tag_id
+//         },
+//         select: {
+//             barcode: true,
+//             activation_code: true
+//         }
+//     });
 
-    if (!items) {
-        res.status(404).send("Not Found");
-    }
-    res.render("download", { items })
-}));
+//     if (!items) {
+//         res.status(404).send("Not Found");
+//     }
+//     res.render("download", { items })
+// }));
 
 
 
